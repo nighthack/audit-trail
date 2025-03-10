64426f9ca59bbd12cb9c289bb468f93fa7e02e29
Vinayaka-nh <165910223+Vinayaka-nh@users.noreply.github.com>
2025-03-10 13:14:28 +0530

    passing dynamic index values during init (#4)

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 "use strict";
-const repository  = new (require("./src/ElasticRepository"))();
-const {DateTime} = require("luxon");
+const ElasticRepository = require("./src/ElasticRepository");
 
-const trail = async (message, action, meta = {}) => {
-    const payload = {
-        service: process.env.APP_NAME || "audit-trail",
-        message,
-        action,
-        ...meta,
-        timestamp: DateTime.local().toMillis()
+const { DateTime } = require("luxon");
+
+const initRepository = (indexName) => {
+    const repository = new ElasticRepository(indexName);
+
+    const trail = async (message, action, meta = {}) => {
+        const payload = {
+            service: process.env.APP_NAME || "audit-trail",
+            message,
+            action,
+            ...meta,
+            timestamp: DateTime.local().toMillis(),
+        };
+
+        console.log("Payload to elastic search", payload);
+        return repository.create(payload);
     };
 
-    console.log("Payload to elastic search", payload);
-
-    return repository.create(payload);
-};
-
-
-let customQuery = async (body) => {
-    return await repository.search(body);
-};
-
-let fetch = async (query, from, size, keyword) => {
-    let body = {query: {bool:{}}}, must = [];
-    must = repository.appendMultiplePropertyMatch(query);
-    if (from && size) {
-        body.from = from;
-        body.size = size;
-    }
-    body.query.bool = {
-        must
+    let customQuery = async (body) => {
+        return await repository.search(body);
     };
 
-    return await repository.search(body);
+    let fetch = async (query, from, size) => {
+        let body = { query: { bool: {} } }, must = [];
+        must = repository.appendMultiplePropertyMatch(query);
+        if (from && size) {
+            body.from = from;
+            body.size = size;
+        }
+        body.query.bool = { must };
+
+        return await repository.search(body);
+    };
+
+    let deleteIndex = async () => {
+        return repository.deleteIndex();
+    };
+
+    return {
+        trail,
+        fetch,
+        customQuery,
+        deleteIndex
+    };
 };
 
-let deleteIndex = async () => {
-    return repository.deleteIndex();
-};
-module.exports = {
-    trail,
-    fetch,
-    customQuery,
-    deleteIndex
-};
+module.exports = initRepository;
